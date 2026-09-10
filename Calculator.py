35e7e9c7d02064015f4a51e2d76ebca34883f8a8
Rosta Gábor <rosta.gabor@hbsz.edu.hu>
2026-09-10 19:18:57 +0200

    GUI has been added!
    UPDATE!
    
    I have added a GUI to my Calculator App,
    It doesnt work for now, I am fixing it!👷

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,83 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title('Calculator')
+window.geometry('505x630')
+window.resizable(False, False)
+window.config(bg='#0f0f0f')
+
+display = tk.Entry(
+    window,
+    font=('Arial', 32),
+    justify='right',
+    bg="#222222"
+)
+
+display.grid(
+    row=0,
+    column=0,
+    columnspan=4,
+    padx=10,
+    pady=20,
+    ipady=15
+)
+
+def button_clicks(value):
+    print(value) 
+
+buttons = [
+    ("C", 1, 0),
+    ("del", 1, 1),
+    ("%", 1, 2),
+    ("÷", 1, 3),
+    
+    ("1", 2, 0),
+    ("2", 2, 1),
+    ("3", 2, 2),
+    ("×", 2, 3),
+    
+    ("4", 3, 0),
+    ("5", 3, 1),
+    ("6", 3, 2),
+    ("−", 3, 3),
+    
+    ("7", 4, 0),
+    ("8", 4, 1),
+    ("9", 4, 2),
+    ("+", 4, 3),
+    
+    ("()", 5, 0),
+    ("0", 5, 1),
+    (".", 5, 2),
+    ("=", 5, 3),
+]
+
+for text, row, column in buttons:
+
+    button = tk.Button(
+        window,
+        text=text,
+        font=("Arial", 20),
+        width=5,
+        height=2,
+
+        bg="#333333",
+        fg="white",
+        activebackground="#555555",
+        activeforeground="white",
+
+        command=lambda value=text: button_click(value)
+    )
+
+    button.grid(
+        row=row,
+        column=column,
+        padx=5,
+        pady=5
+    )
+
+window.mainloop()
+
 num1 = int(input('What is your first number? '))
 num2 = int(input('What is your second number? '))
 operation = input('What is your operation (+, -, *, /)? ')
@@ -10,7 +90,11 @@ def calculation(num1, operation, num2):
     elif operation in('*'):
         return num1 * num2
     elif operation in('/'):
+        if num2 == 0:
+            print('You cant divide by zero!')
+            return num1
         return num1 / num2
+        
     else:
         print('Something Went Wrong!')
         return num1
